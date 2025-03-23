e7a5c66bd5e83dc42973d046587c171f6f356712
Byzgig <davidbhalls@gmail.com>
2025-03-23 17:22:41 -0600

    Final

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,36 @@
 
 class Animal {
   // write your code here
+  static alive = [];
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  hidden = false;
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  bite(herb) {
+    if (herb instanceof Herbivore && !herb.hidden) {
+      herb.health -= 50;
+    }
+
+    if (herb.health <= 0) {
+      Animal.alive = Animal.alive.filter((animal) => animal !== herb);
+      // const idx = Animal.alive.indexOf(herb);
+
+      // Animal.alive.splice(idx, 1);
+    }
+  }
 }
 
 module.exports = {
